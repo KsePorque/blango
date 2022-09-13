665c8b2a174cf9417dcf042446adefc52db6bcd5
Ryabikhina Ksenia <ksenp@list.ru>
2022-09-13 15:33:31 +0000

    played with cache

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,19 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import redirect, render, get_object_or_404
 
+## cache
+#from django.views.decorators.cache import cache_page
+#from django.views.decorators.vary import vary_on_cookie 
+
 from blog.forms import CommentForm
 from blog.models import Post
 
 # create a logger
 logger = logging.getLogger(__name__)
 
+
+#@cache_page(300)
+#@vary_on_cookie
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.info("I am in the index view")
