3998c0b1280a5dffd31c085f1dc9df4dc2b309e7
Ryabikhina Ksenia <ksenp@list.ru>
2022-09-16 14:05:21 +0000

    social authentication with Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -69,15 +69,29 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        "django.contrib.sites",
         'django.contrib.staticfiles',
         'debug_toolbar',
         'blango_auth',
+
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
 
         'crispy_forms',
         'crispy_bootstrap5',
 
         'blog',
     ]
+
+    SITE_ID = 1
+
+    # As custom user model do not have Username field:
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
     CRISPY_TEMPLATE_PACK = 'bootstrap5'
