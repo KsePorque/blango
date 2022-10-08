533a098b6af95044b23983ed2d2d4aeb552f4e10
Ryabikhina Ksenia <ksenp@list.ru>
2022-10-08 09:24:24 +0000

    simple оце added

--- blango/settings.py
+++ blango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 from configurations import Configuration, values
 import dj_database_url
@@ -92,6 +93,11 @@ class Dev(Configuration):
 
     SITE_ID = 1
 
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
+
     REST_FRAMEWORK = {
       "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
@@ -100,6 +106,7 @@ class Dev(Configuration):
           "rest_framework.authentication.BasicAuthentication",
           "rest_framework.authentication.SessionAuthentication",
           "rest_framework.authentication.TokenAuthentication",
+          "rest_framework_simplejwt.authentication.JWTAuthentication",
       ],
 
       "DEFAULT_THROTTLE_CLASSES": [
