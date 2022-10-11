face6e760659ae82bd6a982e68100994fee4155a
Ryabikhina Ksenia <ksenp@list.ru>
2022-10-11 11:42:42 +0000

    test javascript added

--- blango/urls.py
+++ blango/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("", blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
+    path("post-table/", blog.views.post_table, name="blog-post-table"),
 
     path("accounts/", include("django.contrib.auth.urls")),
     
