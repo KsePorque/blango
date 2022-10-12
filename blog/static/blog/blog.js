f6b4bca1a5c6cd1a51378ac12709b3a71024d72f
Ryabikhina Ksenia <ksenp@list.ru>
2022-10-12 12:55:37 +0000

    jsx with babel, adding elements to html

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,3 +1,93 @@
+class PostRow extends React.Component {
+  render () {
+    const post = this.props.post
+
+    let thumbnail
+
+    if (post.hero_image.thumbnail) {
+      thumbnail = <img src={post.hero_image.thumbnail}/>
+    } else {
+      thumbnail = '-'
+    }
+
+    return <tr>
+      <td>{post.title}</td>
+      <td>
+        {thumbnail}
+      </td>
+      <td>{post.tags.join(', ')}</td>
+      <td>{post.slug}</td>
+      <td>{post.summary}</td>
+      <td><a href={'/post/' + post.slug + '/'}>View</a></td>
+    </tr>
+  }
+}
+
+class PostTable extends React.Component {
+  state = {
+    dataLoaded: true,
+    data: {
+      results: [
+        {
+          id: 15,
+          tags: [
+            'django', 'react'
+          ],
+          'hero_image': {
+            'thumbnail': '/media/__sized__/hero_images/smart_house-thumbnail-100x100-70.jpg',
+            'full_size': '/media/hero_images/smart_house.png'
+          },
+          title: 'Test Post',
+          slug: 'test-post',
+          summary: 'A test post, created for Django/React.'
+        }
+      ]
+    }
+  }
+
+  render () {
+    let rows
+    if (this.state.dataLoaded) {
+      if (this.state.data.results.length) {
+        rows = this.state.data.results.map(post => <PostRow post={post} key={post.id}/>)
+      } else {
+        rows = <tr>
+          <td colSpan="6">No results found.</td>
+        </tr>
+      }
+    } else {
+      rows = <tr>
+        <td colSpan="6">Loading&hellip;</td>
+      </tr>
+    }
+
+    return <table className="table table-striped table-bordered mt-2">
+      <thead>
+      <tr>
+        <th>Title</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Slug</th>
+        <th>Summary</th>
+        <th>Link</th>
+      </tr>
+      </thead>
+      <tbody>
+      {rows}
+      </tbody>
+    </table>
+  }
+}
+
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(PostTable),
+  domContainer
+)
+
+
+// Just a sample button
+/*
 class ClickButton extends React.Component {
   state = {
     wasClicked: false
@@ -17,16 +107,17 @@ class ClickButton extends React.Component {
     else
       buttonText = 'Click Me'
 
-    return React.createElement(
-      'button',
-      {
-        className: 'btn btn-primary mt-2',
-        onClick: () => {
+    return <button
+      className="btn btn-primary mt-2"
+      onClick={
+        () => {
           this.handleClick()
         }
-      },
-      buttonText
-    )
+      }
+    >
+      {buttonText}
+    </button>
+  
   }
 }
 
@@ -35,3 +126,4 @@ ReactDOM.render(
   React.createElement(ClickButton),
   domContainer
 )
+*/
